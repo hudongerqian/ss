443435f6f05741cc7ed9aeac1d6d3f6d9b500004
zhangfaqiang <han19980909@163.com>
2019-10-07 15:25:02 +0800

    测试提交没有实际意义

---
+++ src/main/java/com/cq/controller/loginController.java
@@ -0,0 +1,19 @@
+package com.cq.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class loginController {
+
+    @GetMapping("/home")
+    public ModelAndView home() {
+        ModelAndView mv=new ModelAndView();
+        mv.setViewName("home");
+       // return "home";
+        return mv;
+    }
+
+}
